6b65c651f10f49d6c4784538b788fd55f4ba2059
ananya2023 <ananya.miriyala474@gmail.com>
2025-04-07 21:47:53 +0530

    Created some apis

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDb  = require('./config/database')
 const User = require('./models/user.model')
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.json())
@@ -18,6 +19,68 @@ app.post("/signup" , async (req,res)=>{
     }
    
     
+});
+
+// Get all users by email
+app.get("/email" , async (req,res)=>{
+    const userObj = req.body.emailId;
+    console.log(userObj)
+    try {
+        const data = await User.find({emailId : userObj})
+        if(data.length === 0){
+            res.status(400).send("User Not Found")
+        }
+        else {
+            res.status(200).send({msg : "User Fetched Succesfully" , data : data}) 
+        }
+    } catch (error) {
+        res.status(400).send("User not found",error)
+    }
+    
+});
+
+// delete user
+app.delete("/user" , async (req,res)=>{
+    const userObj = req.body.userId;
+    console.log(userObj)
+    try {
+        const data = await User.findByIdAndDelete(userObj)
+        res.status(200).send({msg : "User Deleted Succesfully"})
+    } catch (error) {
+        res.status(400).send("User not found",error)
+    }
+    
+});
+
+
+// update user
+app.patch("/user" , async (req,res)=>{
+    const userId = req.body.userId;
+    console.log(mongoose.Types.ObjectId.isValid(userId)) // should return true
+
+    const data = req.body;
+    console.log(userId )
+    console.log(data)
+    try {
+        const result = await User.findByIdAndUpdate(userId , data) 
+        res.status(200).send({msg : "User Updated Succesfully"})
+    } catch (error) {
+        console.log(error)
+        res.status(400).send("User not found",error)
+    }
+    
+});
+
+
+// Get all users from database
+app.get("/feed" , async (req,res)=>{
+    try {
+        const data = await User.find({})
+        console.log(data)
+        res.status(200).send({msg : "User Fetched Succesfully" , data : data}) 
+    } catch (error) {
+        res.status(400).send("Error in fetching users",error)
+    }
 });
 
 connectDb().then(()=>{
