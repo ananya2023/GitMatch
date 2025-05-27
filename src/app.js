259ca4cfc1e98cfff5d4db51864d7009c0e4fa03
ananya2023 <ananya.miriyala474@gmail.com>
2025-05-27 11:19:44 +0530

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const connectDb  = require('./config/database')
 const User = require('./models/user.model')
 const cookieParser = require('cookie-parser')
@@ -9,12 +10,17 @@ const requestRouter = require("./routes/requests")
 const userRouter = require("./routes/user")
 
 const app = express()
+
 app.use(express.json())
 app.use(cookieParser())
 app.use("/", authRouter)
 app.use("/", profileRouter)
 app.use("/", requestRouter)
 app.use("/",userRouter)
+app.use(cors({
+    origin : "http://localhost:5173/" , 
+    credentials : true
+}))
 
 
 connectDb().then(()=>{
